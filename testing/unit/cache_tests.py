45ef2f2035cb71a239a6d6c8fd64208226631e12
zvxr <zvxr.zppbaaryy@gmail.com>
2017-05-23 16:01:42 -0700

    add cache unit test

---
+++ testing/unit/cache_tests.py
@@ -0,0 +1,31 @@
+
+import tickets.app.cache as cache
+import unittest
+from unittest.mock import Mock, patch
+
+
+class TestCacheMethods(unittest.TestCase):
+    def setUp(self):
+        # Reset cache client. Mock out the pool.
+        global cache
+        reload(cache)
+        cache._redis_client = None
+        self.redis_client_pool = Mock()
+        cache._redis_client_pool = self.redis_client_pool
+
+    @patch('redis.Redis')
+    def test_get_client(self, redis_mock):
+        client = cache.get_client()
+
+        self.assertEqual(client, redis_mock.return_value)
+        redis_mock.assert_called_with(connection_pool=self.redis_client_pool)
+
+    @patch('tickets.app.cache.get_client')
+    def test_ping(self, get_client_mock):
+        client_mock = Mock()
+        get_client_mock.return_value = client_mock
+        resp = cache.ping()
+
+        self.assertEqual(resp, client_mock.ping.return_value)
+        get_client_mock.assert_called_with()
+        client_mock.ping.assert_called_with()

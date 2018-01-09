afedab285540b8af56102744806ae51c1810ad17
zvxr <zvxr.zppbaaryy@gmail.com>
2018-01-09 15:03:08 -0800

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tickets",
-    version='0.2.1',
+    version="0.2.2",
     author="Mike McConnell",
     author_email="djrahl84@gmail.com",
     description=("A lightweight HTTP service that facilitates issuing "

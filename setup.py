3025e70e975025891bb30f9f152788fb71d7e95c
zvxr <zvxr.zppbaaryy@gmail.com>
2019-10-05 09:59:41 -0700

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup, find_packages
 setup(
     name="tickets",
     version="0.2.2",
-    author="Mike McConnell",
     author_email="zvxr.zppbaaryy@gmail.com",
     description=("A lightweight HTTP service that facilitates issuing "
         "and redeeming tickets."),

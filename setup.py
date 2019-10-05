350fee1916da56ea31b50666804abfc4c87e7724
zvxr <zvxr.zppbaaryy@gmail.com>
2019-10-05 09:58:02 -0700

    update email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="tickets",
     version="0.2.2",
     author="Mike McConnell",
-    author_email="djrahl84@gmail.com",
+    author_email="zvxr.zppbaaryy@gmail.com",
     description=("A lightweight HTTP service that facilitates issuing "
         "and redeeming tickets."),
     url="https://github.com/zvxr/tickets",
